f2eb67b081cbfe0f9dc378689e344084fdd15e4a
socrates86 <morrisonikechukwu86@gmail.com>
2023-03-14 15:18:51 +0100

    made some changes

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img src="img" alt="img" className="header--image"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
